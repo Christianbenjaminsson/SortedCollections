4d5169dc20823874e897fbf7974ec356eb81cb61
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-05-26 15:48:10 +0200

    Add more methods

--- src/com/example/sortedCollections/StockItem.java
+++ src/com/example/sortedCollections/StockItem.java
@@ -8,7 +8,13 @@ public class StockItem implements Comparable<StockItem> {
     public StockItem(String name, double price) {
         this.name = name;
         this.price = price;
-        this.quantityStock = 0;
+        this.quantityStock = 0; // or here (but you wouldn´t normally do both
+    }
+
+    public StockItem(String name, double price, int quantityStock) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = quantityStock;
     }
 
     public String getName() {
@@ -46,7 +52,31 @@ public class StockItem implements Comparable<StockItem> {
             return false;
         }
 
-        String objName =((StockItem) obj).getName();
+        String objName = ((StockItem) obj).getName();
         return this.name.equals(objName);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + 31;
+    }
+
+    @Override
+    public int compareTo(StockItem o) {
+        System.out.println("Entering StockItem.compareTo");
+        if (this == o) {
+            return 0;
+        }
+
+        if (o != null) {
+            return this.name.compareTo(o.getName());
+        }
+
+        throw new NullPointerException();
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " : price " + this.price;
     }
 }
