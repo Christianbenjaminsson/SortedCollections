ff89ba231bedde079d1f5403294e9a26ce2e221e
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-05-29 12:17:33 +0200

    Add class Stocklist

--- src/com/example/sortedCollections/StockItem.java
+++ src/com/example/sortedCollections/StockItem.java
@@ -25,7 +25,7 @@ public class StockItem implements Comparable<StockItem> {
         return price;
     }
 
-    public int getStockQuantity() {
+    public int quantityInStock() {
         return quantityStock;
     }
 
